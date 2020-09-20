b12be9435a47db808c2853f2e9b9767ce2d08532
lixiaoyan <1484973645@qq.com>
2020-09-20 20:30:35 +0800

    提交页面

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from '@/views/index'
+import analysis from '@/views/dashboard/analysis'
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+            path: '/',
+            name: 'analysis',
+            component: analysis
+        },
+        {
+            path: '/index',
+            name: 'Index',
+            component: index,
+            children: [{
+                path: '/analysis', //以“/”开头的嵌套路径会被当作根路径，所以子路由上不用加“/”;在生成路由时，主路由上的path会被自动添加到子路由之前，所以子路由上的path不用在重新声明主路由上的path了。
+                name: 'analysis',
+                component: analysis
+            }]
+        }
+    ]
+})
---
+++ src/views/dashboard/analysis.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    1111111111
+  </div>
+</template>
+<script>
+export default {
+    name:"analysis",
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+  
+</style>
